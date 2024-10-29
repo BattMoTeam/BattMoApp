4225d48b508d6c76573089a34ed0845ccbe6ccee
Lorena Hendrix <lorenahendrix163@gmail.com>
2024-10-29 17:35:50 +0100

    changed certbot nginx configuration

--- nginx/renew-certs.sh
+++ nginx/renew-certs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-certbot certonly --webroot -w /usr/share/nginx/html -d app.batterymodel.com --email lorena.hendrix@sintef.no --agree-tos --non-interactive
-
-
+# Renew certificates
+certbot certonly --webroot -w /usr/share/nginx/html -d app.batterymodel.com --email lorena.hendrix@sintef.no --agree-tos --non-interactive -v
