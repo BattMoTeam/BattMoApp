caf51af6ce48fd0f5579e97857de917c2176d5b8
Lorena Hendrix <lorenahendrix163@gmail.com>
2024-11-13 08:24:38 +0100

    changed umami url

--- gui/app.py
+++ gui/app.py
@@ -44,11 +44,15 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+# components.html(
+#     """
+#     <script async src="https://app.batterymodel.com/umami/script.js" data-website-id="adcac53d-bc65-4ca3-9f98-be5c7c4ee75d"></script>
+#     """,
+#     height=0,
+# )
+
 components.html(
-    """
-    <script async src="https://app.batterymodel.com/umami/script.js" data-website-id="adcac53d-bc65-4ca3-9f98-be5c7c4ee75d"></script>
-    """,
-    height=0,
+    '<script async defer data-website-id="adcac53d-bc65-4ca3-9f98-be5c7c4ee75d" src="<https://app.batterymodel.com/umami.js>"></script>'
 )
 
 ##############################
