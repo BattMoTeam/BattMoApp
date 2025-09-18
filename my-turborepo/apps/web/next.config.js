ddb0f785d7dec12ae6878958d668740a7b11b80d
Turbobot <turbobot@vercel.com>
2025-09-18 14:41:34 +0200

    feat(create-turbo): apply official-starter transform

--- my-turborepo/apps/web/next.config.js
+++ my-turborepo/apps/web/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  reactStrictMode: true,
+  transpilePackages: [
+    "@repo/ui",
+  ],
+}
+
 export default nextConfig;
