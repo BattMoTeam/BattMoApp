d820030cf116a0df684546ae04f629fcd280a05d
Lorena Hendrix <lorenahendrix163@gmail.com>
2025-09-30 13:28:07 +0200

    remove old nextjs project

--- nextjs/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
-      <p className="mt-4">Use the navigation bar to explore other pages.</p>
-    </div>
-  );
-}
